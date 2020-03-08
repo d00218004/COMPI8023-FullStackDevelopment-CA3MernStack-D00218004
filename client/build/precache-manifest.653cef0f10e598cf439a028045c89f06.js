46df72bb55fd39858795dbc6b3645c1d796d6016
Matthew Walller <d00218004@student.dkit.ie>
2020-03-08 20:32:32 +0000

    .

---
+++ client/build/precache-manifest.653cef0f10e598cf439a028045c89f06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8cfb4ae75c172d86795a559ef725b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "569e3301d13a7619e91f",
+    "url": "/static/css/main.94af6f2b.chunk.css"
+  },
+  {
+    "revision": "045bf64fdcf17558f89b",
+    "url": "/static/js/2.e757901d.chunk.js"
+  },
+  {
+    "revision": "1fedcf5c84e2f9b04a7e15ecdecddbbe",
+    "url": "/static/js/2.e757901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569e3301d13a7619e91f",
+    "url": "/static/js/main.8c0013f1.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "3bf47a78a5fa8dc4abc95cc8892bbe04",
+    "url": "/static/media/shop-accessories-slider.3bf47a78.jpg"
+  },
+  {
+    "revision": "f3d7f0f43d4590d2633fc47ddde84138",
+    "url": "/static/media/shop-surface-slider.f3d7f0f4.jpg"
+  },
+  {
+    "revision": "fe69acb7d01df8fc0f22cb21023a65d8",
+    "url": "/static/media/shop-xbox-slider.fe69acb7.jpg"
+  },
+  {
+    "revision": "36ece6109609191f36df17a935e159c3",
+    "url": "/static/media/slider5.36ece610.png"
+  },
+  {
+    "revision": "ba1f13ffe12e5a299ca809f0455b3443",
+    "url": "/static/media/slider7.ba1f13ff.png"
+  },
+  {
+    "revision": "56aa9dab54fc82f4fbe4b32a6f10064a",
+    "url": "/static/media/slider8.56aa9dab.png"
+  }
+]);
